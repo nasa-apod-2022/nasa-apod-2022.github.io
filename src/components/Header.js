bd72a37d44b8c2c2486bd739434ab1cb41ef9f03
apoorvsrivastava270 <metalruleskjs@gmail.com>
2022-09-09 23:10:22 +0530

    upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+     return(
+        <div className="w-full flex text-center justify-center items-center text-6xl font-bold p-5">
+            Astronomy Picture of the Day
+        </div>
+     );
+}
+
+export default Header;
